153c38fa3ebe0d75e0b7e066e1e85c58485019a3
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-05-29 09:01:30 -0400

    Add script to download and extract ViVeTool

--- Features/Client OS Features/Disable-NewStartMenu.ps1
+++ Features/Client OS Features/Disable-NewStartMenu.ps1
@@ -1,1 +1,10 @@
+$Vivetool = "https://github.com/thebookisclosed/ViVe/releases/download/v0.3.4/ViVeTool-v0.3.4-IntelAmd.zip"
+$destination = "C:\downloads\Vivetool"
+$ZIP = "C:\downloads\Vivetool\ViVeTool-v0.3.4-IntelAmd.zip"
+$extract = "C:\downloads\Vivetool\extract"
+
+Save-WebFile -SourceUrl $Vivetool -DestinationDirectory $destination
+
+Expand-Archive -Path $ZIP -DestinationPath $extract
+
 
