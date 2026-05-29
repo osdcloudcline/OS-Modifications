2f0eef5b8f9465c54954d10156309641a8445a85
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-05-29 09:11:16 -0400

    Implement feature disable for multiple Windows 11 editions

--- Features/Client OS Features/Disable-NewStartMenu.ps1
+++ Features/Client OS Features/Disable-NewStartMenu.ps1
@@ -11,3 +11,50 @@ $ospath = "C:\OS\Windows11\sources\install.wim"
 $mount = Read-Host -Prompt 'Please enter your path to use as a Mount point'
 
 
+$Index3 = "Windows 11 Professional 25H2"
+
+Write-Host "Processing: $Index3" -ForegroundColor Cyan
+Mount-WindowsImage -ImagePath $ospath -Index 3 -Path $mount
+Write-Host
+
+reg load OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
+
+reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
+reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
+
+cd "C:\downloads\Vivetool\extract"
+.\ViVeTool.exe /disable /id:47205210 /image:"$mount\Windows"
+
+Dismount-WindowsImage -Path "$mount" -Save
+
+$Index6 = "Windows 11 Enterprise 25H2"
+
+Write-Host "Processing: $Index6" -ForegroundColor Cyan
+Mount-WindowsImage -ImagePath $ospath -Index 3 -Path $mount
+Write-Host
+
+reg load OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
+
+reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
+reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
+
+cd "C:\downloads\Vivetool\extract"
+.\ViVeTool.exe /disable /id:47205210 /image:"$mount\Windows"
+
+Dismount-WindowsImage -Path "$mount" -Save
+
+$Index12 = "Windows 11 Pro for Workstations 25H2"
+
+Write-Host "Processing: $Index12" -ForegroundColor Cyan
+Mount-WindowsImage -ImagePath $ospath -Index 3 -Path $mount
+Write-Host
+
+reg load OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
+
+reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
+reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
+
+cd "C:\downloads\Vivetool\extract"
+.\ViVeTool.exe /disable /id:47205210 /image:"$mount\Windows"
+
+Dismount-WindowsImage -Path "$mount" -Save
