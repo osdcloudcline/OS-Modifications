41ea81f5006234dd0bafc56187da61e4e087188e
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-09 07:32:15 -0400

    Add main menu function for OS WIM modifications

--- OSModsMainMenu.ps1
+++ OSModsMainMenu.ps1
@@ -1,1 +1,51 @@
+Function Show-MainMenu{ 
+    [CmdletBinding()]
+    param(
+    [string]$Title = 'OS WIM File Modifications - Main Menu',
+    [string]$Question = 'What type of action do you want to perform?'
+)
+cls
 
+Write-Host "======= $Title ======"
+Write-Host " 1. Add Drivers to WIM File"
+Write-Host " 2. Add Features to WIM File"
+Write-Host " 3. Extract a WIM File from the original install.wim"
+Write-Host " 4. Add Offline WIM File Registry modifications"
+Write-Host " 5. Add Updates to the WIM File"
+Write-Host " 6. Return to Main Menu"
+
+do 
+{
+  $selection = Read-Host 'Please choose an option'
+  switch($selection)
+  {
+
+  '1' { cls
+        $Drivers = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM4/MainMenu.ps1")
+        Invoke-Expression $($Drivers.Content)
+        }
+  '2' { cls
+        $Features = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/AMD/Socket%20AM5/MainMenu.ps1")
+        Invoke-Expression $($Features.Content)
+        }
+  '3' { cls
+        $ExtractWIM = Invoke-WebRequest("https://github.com/osdcloudcline/OSDCloud/raw/refs/heads/main/Scripts/ISO%20Files/Workspace%20Creation%20Scripts/Motherboards/Intel/Socket%20LGA1700/MainMenu.ps1")
+        Invoke-Expression $($ExtractWIM.Content)
+        }
+  '4' { cls
+        $Registry = Invoke-WebRequest("")
+        Invoke-Expression $($Registry.Content)
+        }   
+  '5' { cls
+        $Updates = Invoke-WebRequest("")
+        Invoke-Expression $($Updates.Content)
+        }
+  '6' { cls
+        $Main = Invoke-WebRequest("")
+        Invoke-Expression $($Main.Content)
+        }
+    }
+    }
+     until ($selection -eq '6'){Invoke-Expression $($Main.Content)}
+}
+Show-MainMenu
