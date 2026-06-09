17d2e77ebfce48561fab929630d10ba798acbdf9
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-08 23:41:56 -0400

    Add checks for install.wim file existence

--- Image Extraction/WIMExtract.ps1
+++ Image Extraction/WIMExtract.ps1
@@ -1,2 +1,35 @@
 $ISOFolder = Read-Host -Prompt 'Please enter the source directory'
 $WimFolder = $ISOFolder
+
+if (Test-Path $WimFolder\Sources\install.wim)
+        {
+        $WimCount = 1
+            if (($WIMFolder -match "x86") -or ($WIMFolder -match "x64"))
+            {
+            $ISOFolder = $ISOFolder -replace "....$" 
+            }
+        }
+    elseif (Test-Path $WimFolder)
+        {
+        $WimCount = 0
+        cls
+        Write-Host
+        Write-Host ' No Windows image (install.wim file) found'
+        Write-Host ' Please check path and try again.'
+        Write-Host
+        Pause
+        }
+    else
+        {
+        $FileCount = 0
+        cls
+        Write-Host
+        Write-Host ' Path'$ISOFolder 'does not exist.'
+        Write-Host
+        Write-Host ' ' -NoNewline
+        Pause
+        }
+    }
+
+$WimFile = Join-Path $WimFolder '\Sources\install.wim'
+
