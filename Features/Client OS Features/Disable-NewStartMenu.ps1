85c7191bb476c3fdcbbe2f79570f0f3ebc2a97db
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-11 14:03:46 -0400

    Update Windows image mounting for different editions

--- Features/Client OS Features/Disable-NewStartMenu.ps1
+++ Features/Client OS Features/Disable-NewStartMenu.ps1
@@ -10,7 +10,6 @@ Expand-Archive -Path $ZIP -DestinationPath $extract
 $ospath = "Read-Host -Prompt 'Please enter your path where your extracted OS is'
 $mount = Read-Host -Prompt 'Please enter your path to use as a Mount point'
 
-
 $Index3 = "Windows 11 Professional 25H2"
 
 Write-Host "Processing: $Index3" -ForegroundColor Cyan
@@ -22,9 +21,6 @@ reg load HKLM\OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
 reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
 reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
 
-cd "C:\downloads\Vivetool\extract"
-.\ViVeTool.exe /disable /id:47205210 /image:"$mount\Windows"
-
 reg unload HKLM\OfflineSoftware
 
 Dismount-WindowsImage -Path $mount -Save
@@ -32,16 +28,13 @@ Dismount-WindowsImage -Path $mount -Save
 $Index6 = "Windows 11 Enterprise 25H2"
 
 Write-Host "Processing: $Index6" -ForegroundColor Cyan
-Mount-WindowsImage -ImagePath $ospath -Index 3 -Path $mount
+Mount-WindowsImage -ImagePath $ospath -Index 6 -Path $mount
 Write-Host
 
 reg load HKLM\OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
 
 reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
 reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
-
-cd "C:\downloads\Vivetool\extract"
-.\ViVeTool.exe /disable /id:47205210 /image:"$mount\Windows"
 
 reg unload HKLM\OfflineSoftware
 
@@ -50,16 +43,13 @@ Dismount-WindowsImage -Path $mount -Save
 $Index12 = "Windows 11 Pro for Workstations 25H2"
 
 Write-Host "Processing: $Index12" -ForegroundColor Cyan
-Mount-WindowsImage -ImagePath $ospath -Index 3 -Path $mount
+Mount-WindowsImage -ImagePath $ospath -Index 12 -Path $mount
 Write-Host
 
 reg load HKLM\OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
 
 reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
 reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
-
-cd "C:\downloads\Vivetool\extract"
-.\ViVeTool.exe /disable /id:47205210 /image:"$mount\Windows"
 
 reg unload HKLM\OfflineSoftware
 
