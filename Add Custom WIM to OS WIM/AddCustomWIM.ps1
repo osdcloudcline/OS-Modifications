7ae42ad5d9120923a9a6b2858e14404afc3d2063
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-15 07:36:26 -0400

    Add initial script for custom WIM integration

---
+++ Add Custom WIM to OS WIM/AddCustomWIM.ps1
@@ -0,0 +1,4 @@
+$ISOFolder = Read-Host -Prompt 'Please enter the source directory'
+$WimFolder = $ISOFolder
+$Destination = Read-Host -Prompt 'Please enter destination path for extracted WIM File'
+
