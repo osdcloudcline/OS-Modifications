dd73ec612e751f95c19e4ebf1dd16e4ac4fb8c0c
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-15 07:55:47 -0400

    Refactor WIM handling functions and prompts

--- Add Custom WIM to OS WIM/AddCustomWIM.ps1
+++ Add Custom WIM to OS WIM/AddCustomWIM.ps1
@@ -4,22 +4,30 @@ Write-Host "The Destination Image Location prompt is the actual Windows install.
 Write-Host
 
 
+
 Function Show-CustomExport(){
 $WIMSource = Read-Host -Prompt 'Please enter the source image file location'
 $WIMDestination = Read-Host -Prompt 'Please enter destination path for extracted WIM File'
 
+Write-Host "Adding custom image to WIM" -ForegroundColor Cyan
+Export-WindowsImage -SourceImagePath "$WIMSource" -SourceIndex 1 -DestinationImagePath "WIMDestination" -CompressionType Maximum -CheckIntegrity
+
 $WIMQuestrion = Read-Host -Prompt 'Do you want to add another CUSTOM image to the install.wim file?'
-}
-
-Function Show-WIMInfo(){
-
-}
-
 If(($WIMQuestion -eq "YES") -or ($WIMQuestion -eq "yes") -or ($WIMQuestion -eq "Y") -or ($WIMQuestion -eq "y"){
 Show-CustomExport
 }elseif(($WIMQuestion -eq "NO") -or ($WIMQuestion -eq "no") -or ($WIMQuestion -eq "N") -or ($WIMQuestion -eq "n"){
 Show-WIMInfo
 }
+}
+
+Function Show-WIMInfo(){
+$WIMInfo = Read-Host -Prompt 'Please enter the location for the Windows install.wim file'
+Get-WindowsImage -ImagePath $WIMInfo
+}
+
+Show-CustomExport
+
+
 
 
 
