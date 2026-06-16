5d7a617cccc904eb1b347edd66bccef0414268e7
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-16 14:49:07 -0400

    Remove Vivetool setup from Disable-NewStartMenu.ps1
    Removed Vivetool download and extraction steps.

--- Features/Client OS Features/Disable-NewStartMenu.ps1
+++ Features/Client OS Features/Disable-NewStartMenu.ps1
@@ -1,12 +1,3 @@
-$Vivetool = "https://github.com/thebookisclosed/ViVe/releases/download/v0.3.4/ViVeTool-v0.3.4-IntelAmd.zip"
-$destination = "C:\downloads\Vivetool"
-$ZIP = "C:\downloads\Vivetool\ViVeTool-v0.3.4-IntelAmd.zip"
-$extract = "C:\downloads\Vivetool\extract"
-
-Save-WebFile -SourceUrl $Vivetool -DestinationDirectory $destination
-
-Expand-Archive -Path $ZIP -DestinationPath $extract
-
 
 ##########################################################
 # Checking if PS is running elevated. If not, elevating it
