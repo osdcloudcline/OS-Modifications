702be878dfb901d735f96e1b99bd355d1b0df512
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-16 07:03:58 -0400

    Refactor script to include elevation check and instructions

--- Features/Client OS Features/Disable-NewStartMenu.ps1
+++ Features/Client OS Features/Disable-NewStartMenu.ps1
@@ -7,14 +7,216 @@ Save-WebFile -SourceUrl $Vivetool -DestinationDirectory $destination
 
 Expand-Archive -Path $ZIP -DestinationPath $extract
 
-$ospath = "Read-Host -Prompt 'Please enter your path where your extracted OS is'
-$mount = Read-Host -Prompt 'Please enter your path to use as a Mount point'
 
-$Index3 = "Windows 11 Professional 25H2"
+##########################################################
+# Checking if PS is running elevated. If not, elevating it
+##########################################################   
 
-Write-Host "Processing: $Index3" -ForegroundColor Cyan
-Mount-WindowsImage -ImagePath $ospath -Index 3 -Path $mount
+function Use-RunAs 
+{    
+    # Check if script is running as Administrator and if not elevate it
+    # Use Check Switch to check if admin 
+     
+    param([Switch]$Check) 
+     
+    $IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()` 
+        ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") 
+         
+    if ($Check) { return $IsAdmin }   
+      
+    if ($MyInvocation.ScriptName -ne "") 
+    {  
+        if (-not $IsAdmin)  
+          {  
+            try 
+            {  
+                $arg = "-file `"$($MyInvocation.ScriptName)`"" 
+                Start-Process "$psHome\powershell.exe" -Verb Runas -ArgumentList $arg -ErrorAction 'stop'  
+            } 
+            catch 
+            { 
+                Write-Warning "Error - Failed to restart script elevated"  
+                break               
+            } 
+            exit 
+        }  
+    }  
+} 
+
+Use-RunAs 
+
+Function Show-IntelDrivers(){
+
+##########################################################
+# Show short instructions to user
+##########################################################   
+
+cls
+Write-Host                                                                       
+Write-Host ' This script will update Windows 10 install media with new drivers'
+Write-Host ' downloaded from http://www.catalog.update.microsoft.com'
+Write-Host 
+Write-Host ' Please notice that the process will take quite some time, depending'
+Write-Host ' on amount and size of drivers being applied to Windows image. '
 Write-Host
+Write-Host ' If you already have a bootable Windows 10 install media on USB '
+Write-Host ' flash drive, plug it in now.'
+Write-Host 
+Write-Host ' If you want to upgrade an ISO instead, mount (double click) a Windows'
+Write-Host ' ISO image and copy its content to a folder on local PC, for instance'
+Write-Host ' "D:\ISO_Files". Make sure the folder has no other content.'
+Write-Host 
+Write-Host ' When ISO files have been copied to a hard disk folder, or USB drive'
+Write-Host ' has been plugged in, press Enter to start.'
+Write-Host 
+Write-Host '                                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' Notice that you cannot use this script to update an ESD based install' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' media like for instance ISO / USB made with Media Creation Tool.     ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' You must first convert "install.esd" file to "install.wim". See      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host ' TenForums tutorial "Convert ESD to WIM":' -ForegroundColor DarkBlue -BackgroundColor White -NoNewline
+Write-Host ' http://w10g.eu/esd2wim      ' -ForegroundColor DarkCyan -BackgroundColor White
+Write-Host '                                                                      ' -ForegroundColor DarkBlue -BackgroundColor White
+Write-Host
+Write-Host ' ' -NoNewline
+pause
+
+##########################################################
+# Delete possible old log files from previous runs
+##########################################################
+
+if (Test-Path C:\OSDCloud\Logs\OSDrivers\DriverSuccess.log) {Remove-Item C:\OSDCloud\Logs\OSDrivers\DriverSuccess.log}
+if (Test-Path C:\OSDCloud\Logs\OSDrivers\DriverFail.log) {Remove-Item C:\OSDCloud\Logs\OSDrivers\DriverFail.log}
+
+##########################################################
+# Prompt user for path to install media (USB drive) or 
+# folder where ISO content was copied to.
+#
+# Using 'while' loop to check that source given by user 
+# contains a Windows image, if not user is asked to chek
+# path and try again
+##########################################################
+
+$WimCount = 0
+while ($WimCount -eq 0) {
+cls
+Write-Host 
+Write-Host ' Enter source path. In case you are using a plugged in USB flash'
+Write-Host ' drive, simply enter its drive letter followed by : (colon).'
+Write-Host
+Write-Host ' If the source you are using is a Windows 10 ISO or DVD, enter.'
+Write-Host ' path to folder where you copied ISO / DVD content.'
+Write-Host 
+Write-Host 
+Write-Host ' Examples:'
+Write-Host ' - A USB drive, enter its drive letter with colon (D: or F:)'
+Write-Host ' - A USB drive with both bit versions, enter D:\x86 or D:\x64'
+Write-Host ' - ISO files copied to folder, enter path (D:\ISO_Files)'
+Write-Host ' - Dual bit version ISO copied to folder, enter path with bit version'
+Write-Host '   (W:\MyISOFolder\x86 or W:\MyISOFolder\x64)' 
+Write-Host
+
+$ISOFolder = Read-Host -Prompt ' Enter source, press Enter'
+$WimFolder = $ISOFolder
+   
+    if (Test-Path $WimFolder\Sources\install.wim)
+        {
+        $WimCount = 1
+            if (($WIMFolder -match "x86") -or ($WIMFolder -match "x64"))
+            {
+            $ISOFolder = $ISOFolder -replace "....$" 
+            }
+        }
+    elseif (Test-Path $WimFolder)
+        {
+        $WimCount = 0
+        cls
+        Write-Host
+        Write-Host ' No Windows image (install.wim file) found'
+        Write-Host ' Please check path and try again.'
+        Write-Host
+        Pause
+        }
+    else
+        {
+        $FileCount = 0
+        cls
+        Write-Host
+        Write-Host ' Path'$ISOFolder 'does not exist.'
+        Write-Host
+        Write-Host ' ' -NoNewline
+        Pause
+        }
+    }
+
+$WimFile = Join-Path $WimFolder '\Sources\install.wim'
+
+##########################################################
+# List Windows editions on image, prompt user for
+# edition to be updated
+##########################################################
+
+cls
+Get-WindowsImage -ImagePath $WimFile | Format-Table ImageIndex, ImageName
+Write-Host 
+Write-Host ' The install.wim file contains above listed Windows editions.'
+Write-Host ' Which edition should be updated?'
+Write-Host  
+Write-Host ' Enter the ImageIndex number of correct edition and press Enter.'
+Write-Host ' If this is a single edition Windows image, enter 1.'                                                                     
+Write-Host
+$Index = Read-Host -Prompt ' Select edition'
+
+##########################################################
+# Ask user which drive should be used for temporary 
+# working folder 'Mount'. If 'Mount' exists on selected
+# drive, delete and recreate it.
+##########################################################
+
+cls
+Write-Host
+[System.IO.DriveInfo]::GetDrives() | Where-Object {$_.DriveType -eq 'Fixed'} | Format-Table @{n='Drive ID';e={($_.Name)}}, @{n='Label';e={($_.VolumeLabel)}}, @{n='Free (GB)';e={[int]($_.AvailableFreeSpace/1GB)}}
+Write-Host
+Write-Host ' Above is a list of all hard disk partitions showing available'
+Write-Host ' free space on each of them. Select a partition for temporary'
+Write-Host ' folder to mount Windows image. Selected partition must have at'
+Write-Host ' least 15 GB available free space. Folder will be removed when'
+Write-Host ' image has been updated.'
+Write-Host
+$Drive = Read-Host -Prompt ' Enter drive letter and press Enter'
+$Mount = $Drive.SubString(0,1) + ':\Mount'
+
+if (Test-Path $Mount) {Remove-Item $Mount}
+$Mount = New-Item -ItemType Directory -Path $Mount
+
+##########################################################
+# Mount Windows image in temporary mount folder.
+#
+# Adding eight empty lines to $EmptySpace variable to be
+# used as placeholder to push output below PowerShell
+# progressbar which is shown on top. Five empty lines would
+# be enough for PowerShell ISE but standard PowerShell will
+# need eight lines, otherwise output remains hidden
+##########################################################
+
+cls
+$EmptySpace = @"
+
+
+
+  
+ 
+
+
+
+"@
+
+Write-Host $EmptySpace
+Write-Host ' Mounting Windows image. This will take a few minutes.'
+Mount-WindowsImage -ImagePath $WimFolder\Sources\install.wim -Index $Index -Path $Mount | Out-Null
+Write-Host
+Write-Host ' Image mounted, modifying image.'
+Write-Host
+
 
 reg load HKLM\OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
 
@@ -23,34 +225,6 @@ reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement
 
 reg unload HKLM\OfflineSoftware
 
-Dismount-WindowsImage -Path $mount -Save
 
-$Index6 = "Windows 11 Enterprise 25H2"
 
-Write-Host "Processing: $Index6" -ForegroundColor Cyan
-Mount-WindowsImage -ImagePath $ospath -Index 6 -Path $mount
-Write-Host
 
-reg load HKLM\OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
-
-reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
-reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
-
-reg unload HKLM\OfflineSoftware
-
-Dismount-WindowsImage -Path $mount -Save
-
-$Index12 = "Windows 11 Pro for Workstations 25H2"
-
-Write-Host "Processing: $Index12" -ForegroundColor Cyan
-Mount-WindowsImage -ImagePath $ospath -Index 12 -Path $mount
-Write-Host
-
-reg load HKLM\OfflineSoftware "$mount\Windows\System32\config\SOFTWARE"
-
-reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "EnabledState" /t REG_DWORD /d 1 /f
-reg add "HKLM\OfflineSoftware\Microsoft\Windows\CurrentVersion\FeatureManagement\Overrides\0\47205210" /v "StateOptions" /t REG_DWORD /d 1 /f
-
-reg unload HKLM\OfflineSoftware
-
-Dismount-WindowsImage -Path $mount -Save
