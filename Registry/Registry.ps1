4b728a584068991cc1fba3c32d8869471bf1a800
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-16 22:04:59 -0400

    Update registry commands for Safe Mode options

--- Registry/Registry.ps1
+++ Registry/Registry.ps1
@@ -236,7 +236,9 @@ reg load HKLM\OfflineSystem "$Mount\Windows\System32\config\SYSTEM"
 Write-Host "Importing $HKCRREG1...." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Adding $HKCRREG1..." -Verbose
-reg add "HKLM\OfflineSystem\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" /f
+reg add "HKLM\OfflineSystem\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" /v "Copy To" /f
+reg add "HKLM\OfflineSystem\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB631-2971-11D1-A18C-00C04FD75D13}" /v "Move To" /f
+
 
 reg unload HKLM\OfflineSystem
 
@@ -256,25 +258,21 @@ reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "MUIV
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "Position" /f
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "SubCommands" /t REG_SZ /d "" /f
 
-
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode" /ve /t REG_SZ /d "Restart in Normal Mode" /f
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /deletevalue {default} safeboot & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
-
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command Start-Process cmd -ArgumentList '/c bcdedit /deletevalue {default} safeboot & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs" /f
 
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode" /ve /t REG_SZ /d "Restart in Safe Mode" /f
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {default} safeboot minimal & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
-
+reg.exe add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode\command" /ve /t REG_SZ /d --% "powershell -windowstyle hidden -command Start-Process cmd -ArgumentList '/s' , '/c' , 'bcdedit /set {default} safeboot minimal & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs" /f
 
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking" /ve /t REG_SZ /d "Restart in Safe Mode with Networking" /f
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {default} safeboot network & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
-
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking\command" /ve /t REG_SZ /d "powershell -WindowStyle Hidden -Command Start-Process cmd -ArgumentList '/c bcdedit /set {default} safeboot network && bcdedit /deletevalue {default} safebootalternateshell && shutdown -r -t 00 -f' -Verb RunAs" /f
 
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt" /ve /t REG_SZ /d "Restart in Safe Mode with Command Prompt" /f
 reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {default} safeboot minimal & bcdedit /set {default} safebootalternateshell yes & shutdown -r -t 00 -f' -Verb runAs\"" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \`"Start-Process cmd -ArgumentList '/c bcdedit /set {default} safeboot minimal && bcdedit /set {default} safebootalternateshell yes && shutdown -r -t 00 -f' -Verb runAs\`"" /f
 
 reg unload HKLM\OfflineSoftware
 
@@ -346,7 +344,7 @@ reg unload HKLM\OfflineSystem
 
 $HKLMSYSTEMREG2 = "Disable Windows Defender"
 
-reg load HKLM\OfflineSystem $Mount\Windows\System32\Config\SYSTEM
+reg load HKLM\OfflineSystem "$Mount\Windows\System32\Config\SYSTEM"
 reg load HKLM\OfflineSoftware "$Mount\Windows\System32\config\SOFTWARE"
 
 Write-Host "Importing $HKLMSYSTEMREG2..." -ForegroundColor Cyan
