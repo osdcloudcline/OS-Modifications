5e36a859241b09a38665745be46cbf05e1f45759
osdcloudcline <163425495+osdcloudcline@users.noreply.github.com>
2026-06-16 20:58:23 -0400

    Update registry modifications for offline settings

--- Registry/Registry.ps1
+++ Registry/Registry.ps1
@@ -231,14 +231,16 @@ Write-Host
 
 $HKCRREG1 = "Add Copy To Move To"
 
-reg load HKLM\OfflineSettings $Mount\Windows\System32\Config\Software
+reg load HKLM\OfflineSystem "$Mount\Windows\System32\config\SYSTEM"
 
 Write-Host "Importing $HKCRREG1...." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Adding $HKCRREG1..." -Verbose
-reg add "HKLM\OfflineSoftware\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" /f
+reg add "HKLM\OfflineSystem\Classes\AllFilesystemObjects\shellex\ContextMenuHandlers\{C2FBB630-2971-11D1-A18C-00C04FD75D13}" /f
 
-pause
+reg unload HKLM\OfflineSystem
+
+reg load HKLM\OfflineSoftware "$Mount\Windows\System32\config\SOFTWARE"
 
 $HKCRREG2 = "Add Safe Mode"
 
@@ -248,33 +250,33 @@ Write-Host "Importing $HKCRREG2..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Adding $HKCRREG2..." -Verbose
 
-:: Base SafeMode Entry
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode" /v "icon" /t REG_SZ /d "bootux.dll,-1032" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode" /v "MUIVerb" /t REG_SZ /d "Safe Mode" /f
-reg delete "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode" /v "Position" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode" /v "SubCommands" /t REG_SZ /d "" /f
 
-:: 001 - Normal Mode
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\001-NormalMode" /ve /t REG_SZ /d "Restart in Normal Mode" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\001-NormalMode" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\001-NormalMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /deletevalue {current} safeboot & bcdedit /deletevalue {current} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "icon" /t REG_SZ /d "bootux.dll,-1032" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "MUIVerb" /t REG_SZ /d "Safe Mode" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "Position" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode" /v "SubCommands" /t REG_SZ /d "" /f
 
-:: 002 - Safe Mode
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\002-SafeMode" /ve /t REG_SZ /d "Restart in Safe Mode" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\002-SafeMode" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\002-SafeMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {current} safeboot minimal & bcdedit /deletevalue {current} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
 
-:: 003 - Safe Mode with Networking
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking" /ve /t REG_SZ /d "Restart in Safe Mode with Networking" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {current} safeboot network & bcdedit /deletevalue {current} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode" /ve /t REG_SZ /d "Restart in Normal Mode" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode" /v "HasLUAShield" /t REG_SZ /d "" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\001-NormalMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /deletevalue {default} safeboot & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
 
-:: 004 - Safe Mode with Command Prompt
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt" /ve /t REG_SZ /d "Restart in Safe Mode with Command Prompt" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt" /v "HasLUAShield" /t REG_SZ /d "" /f
-reg add "HKLM\SOFTWARE\Classes\OFFLINE\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {current} safeboot minimal & bcdedit /set {current} safebootalternateshell yes & shutdown -r -t 00 -f' -Verb runAs\"" /f
 
-pause
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode" /ve /t REG_SZ /d "Restart in Safe Mode" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode" /v "HasLUAShield" /t REG_SZ /d "" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\002-SafeMode\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {default} safeboot minimal & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
+
+
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking" /ve /t REG_SZ /d "Restart in Safe Mode with Networking" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking" /v "HasLUAShield" /t REG_SZ /d "" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\003-SafeModeNetworking\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {default} safeboot network & bcdedit /deletevalue {default} safebootalternateshell & shutdown -r -t 00 -f' -Verb runAs\"" /f
+
+
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt" /ve /t REG_SZ /d "Restart in Safe Mode with Command Prompt" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt" /v "HasLUAShield" /t REG_SZ /d "" /f
+reg add "HKLM\OfflineSoftware\Classes\DesktopBackground\Shell\SafeMode\shell\004-SafeModeCommandPrompt\command" /ve /t REG_SZ /d "powershell -windowstyle hidden -command \"Start-Process cmd -ArgumentList '/s,/c,bcdedit /set {default} safeboot minimal & bcdedit /set {default} safebootalternateshell yes & shutdown -r -t 00 -f' -Verb runAs\"" /f
+
+reg unload HKLM\OfflineSoftware
 
 $HKLMREG1 = "Adobe Master Collection Suite"
 
@@ -282,34 +284,34 @@ Write-Host "Importing $HKLMREG1..." -ForegroundColor Cyan
 Write-Host 
 Write-Verbose "Adding $HKLMREG1..." -Verbose
 
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cServices" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cCloud" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultExecMenuItems" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultFindAttachmentPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchAttachmentPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cIPM" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cSharePoint" /f
-reg add "HKLM\OFFLINE\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cWebmailProfiles" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cServices" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cCloud" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultExecMenuItems" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultFindAttachmentPerms" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchAttachmentPerms" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cIPM" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cSharePoint" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cWebmailProfiles" /f
 
-reg add "HKLM\OFFLINE\SOFTWARE\Wow6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cCloud" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Wow6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Wow6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cIPM" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Wow6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Wow6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cSharePoint" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Wow6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWebmailProfiles" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cCloud" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cIPM" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cSharePoint" /f
+reg add "HKLM\OfflineSoftware\WOW6432Node\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWebmailProfiles" /f
 
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cCloud" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cIPM" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cSharePoint" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWebmailProfiles" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cCloud" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cIPM" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cServices" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cSharePoint" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Acrobat Reader\DC\FeatureLockDown\cWebmailProfiles" /f
 
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultExecMenuItems" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultFindAttachmentPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchAttachmentPerms" /f
-reg add "HKLM\OFFLINE\SOFTWARE\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultExecMenuItems" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultFindAttachmentPerms" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchAttachmentPerms" /f
+reg add "HKLM\OfflineSoftware\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown\cDefaultLaunchURLPerms" /f
 
 pause 
 
@@ -319,20 +321,16 @@ Write-Host "Importing $HKLMREG2..." -ForegroundColor Cyan
 Write-Host 
 Write-Verbose "Adding $HKLMREG2..." -Verbose
 
-:: 1. Disable on-device local AI models (stops Gemini Nano 4GB download)
-reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "GenAILocalFoundationalModelSettings" /t REG_DWORD /d 1 /f
 
-:: 2. Disable generic Generative AI default settings
-reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "GenAiDefaultSettings" /t REG_DWORD /d 2 /f
+reg add "HKLM\OfflineSoftware\Policies\Google\Chrome" /v "GenAILocalFoundationalModelSettings" /t REG_DWORD /d 1 /f
+reg add "HKLM\OfflineSoftware\Policies\Google\Chrome" /v "GenAiDefaultSettings" /t REG_DWORD /d 2 /f
+reg add "HKLM\OfflineSoftware\Policies\Google\Chrome" /v "AIModeSettings" /t REG_DWORD /d 1 /f
 
-:: 3. Disable AI Mode buttons/Omnibox integrations
-reg add "HKLM\SOFTWARE\Policies\Google\Chrome" /v "AIModeSettings" /t REG_DWORD /d 1 /f
-
-reg unload HKLM\OfflineSettings
+reg unload HKLM\OfflineSoftware
 
 pause
 
-reg load HKLM\OfflineSystem $Mount\Windows\System32\Config\System
+reg load HKLM\OfflineSystem $Mount\Windows\System32\Config\SYSTEM
 
 $HKSYSTEMREG1 = "Disable BitLocker Device Encryption"
 
@@ -342,20 +340,29 @@ Write-Host
 Write-Verbose "Adding $HKSYSTEMREG1..." -Verbose
 reg add "HKLM\OfflineSystem\ControlSet001\Control\BitLocker" /v "PreventDeviceEncryption" /t REG_DWORD /d 1 /f
 
+reg unload HKLM\OfflineSystem
+
+
+
 $HKLMSYSTEMREG2 = "Disable Windows Defender"
+
+reg load HKLM\OfflineSystem $Mount\Windows\System32\Config\SYSTEM
+reg load HKLM\OfflineSoftware "$Mount\Windows\System32\config\SOFTWARE"
 
 Write-Host "Importing $HKLMSYSTEMREG2..." -ForegroundColor Cyan
 Write-Host
 Write-Verbose "Adding $HKLMSYSTEMREG2..." -Verbose
 
-reg add "HKLM\OfflineSettings\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
-reg add "HKLM\OfflineSettings\SYSTEM\CurrentControlSet\Services\Sense" /v Start /t REG_DWORD /d 4 /f
-reg add "HKLM\OfflineSettings\SYSTEM\CurrentControlSet\Services\WdFilter" /v Start /t REG_DWORD /d 4 /f
-reg add "HKLM\OfflineSettings\SYSTEM\CurrentControlSet\Services\WdBoot" /v Start /t REG_DWORD /d 4 /f
-reg add "HKLM\OfflineSettings\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f 
+reg add "HKLM\OfflineSoftware\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+reg add "HKLM\OfflineSettings\CurrentControlSet\Services\Sense" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\OfflineSystem\CurrentControlSet\Services\WdFilter" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\OfflineSystem\CurrentControlSet\Services\WdBoot" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\OfflineSoftware\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f 
+
 reg add "HKLM\OfflineSystem\ControlSet001\Services\WinDefend" /v Start /t REG_DWORD /d 4 /f
 
 reg unload HKLM\OfflineSystem
+reg unload HKLM\OfflineSoftware
 
 pause
 
